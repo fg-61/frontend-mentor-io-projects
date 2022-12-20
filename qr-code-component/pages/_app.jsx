3e5b98271a7dce84130105a0d06210d89faf6248
Fatih Gören <fatihgoren61@gmail.com>
2022-12-20 12:25:34 +0300

    initial

---
+++ qr-code-component/pages/_app.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>QR Component</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
